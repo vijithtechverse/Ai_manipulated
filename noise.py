7181812254864dcc307ea3e53bcee087fb095cf1
vijithtechverse <vijith.kammili@techverse.world>
2026-01-30 16:22:15 +0530

    Refactor noise analysis function for variance calculation

--- noise.py
+++ noise.py
@@ -1,8 +1,7 @@
-import cv2
 import numpy as np
+from PIL import Image
 
-def noise_analysis(image):
-    img = np.array(image.convert("L"))
-    laplacian = cv2.Laplacian(img, cv2.CV_64F)
-    variance = laplacian.var()
-    return variance
+def noise_variance(image_path):
+    img = Image.open(image_path).convert("L")
+    img = np.array(img)
+    return np.var(img)
